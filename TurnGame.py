4c852591b92510af2b5f6d61ac9e14e7327eae2e
Brian Hong <35075001+h0ngry@users.noreply.github.com>
2018-01-08 15:38:56 -0600

    Add files via upload

--- TurnGame.py
+++ TurnGame.py
@@ -24,15 +24,35 @@ class enemy:
 class player:
     def __init__(self,type):
         if (type == 1):
-        type
+            category = "melee"
         moves
         damage range
+
+        if (type == 2):
+            category = "ranged"
+
+        if (type == 3):
+            category = "mage"
+
+        hp = 10
+
+    def death(self):
+        if player.hp <= 0:
+            return True
+        else:
+            return False
 
 
 def main():
 
     typ = eval(input("Which class do you want to be? Press 1 to be a Warrior. Press 2 to be an Archer. Press 3 to be a mage."))
-    player_one = player(1)
+    player_one = player(typ)
+    while not player_one.death():
+        foe = enemy()
+        print("You encounter an enemy and it is of the", enemy.type, "class")
+
+
+
 
 
 
