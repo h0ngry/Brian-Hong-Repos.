7191fe2026d95371ff94451c91d8f78204b50768
Brian Hong <35075001+h0ngry@users.noreply.github.com>
2018-01-08 22:34:36 -0600

    Add files via upload

--- TurnGame.py
+++ TurnGame.py
@@ -2,19 +2,27 @@ import random
 class enemy:
     def __init__(self):
         num = random.randint(1,3)
-        enem_hp = 10
+        self.enem_hp = 10
         if(num==1):
-            type = "melee"
+            self.type = "melee"
         if(num==2):
-            type = "ranged"
+            self.type = "ranged"
         if(num==3):
-            type = "magic"
+            self.type = "magic"
 
-    def dmg(self):
+    def attack(self,player):
 
-        hit = random.randint(1,3)
-        if (hit == 1 or hit == 2):
-            player.hp -= 1
+        self.hit = random.randint(1,3)
+
+        player.hp -= self.hit
+
+    def isDead(self):
+        if self.enem_hp <= 0:
+
+            return True
+        else:
+            return False
+
 
 
 
@@ -25,8 +33,8 @@ class player:
     def __init__(self,type):
         if (type == 1):
             category = "melee"
-        moves
-        damage range
+        #moves
+        #damage range
 
         if (type == 2):
             category = "ranged"
@@ -34,22 +42,57 @@ class player:
         if (type == 3):
             category = "mage"
 
-        hp = 10
+        self.hp = 10
 
-    def death(self):
-        if player.hp <= 0:
+    def isDead(self):
+        if self.hp <= 0:
             return True
         else:
             return False
 
+    def attack(self,enemy):
+        self.damage = random.randint(4,5)
+        enemy.enem_hp -= self.damage
+
+
+
 
 def main():
 
-    typ = eval(input("Which class do you want to be? Press 1 to be a Warrior. Press 2 to be an Archer. Press 3 to be a mage."))
+    typ = eval(input("Which class do you want to be? Press 1 to be a Warrior. Press 2 to be an Archer. Press 3 to be a mage. "))
     player_one = player(typ)
-    while not player_one.death():
+    while not player_one.isDead():
         foe = enemy()
-        print("You encounter an enemy and it is of the", enemy.type, "class")
+        print("You encounter an enemy and it is of the", foe.type, "class")
+        while not foe.isDead():
+            print("")
+            atk = eval(input("Press 1 to attack "))
+            while atk != 1:
+                atk = eval(input("Press 1 to attack"))
+            print("")
+            player_one.attack(foe)
+            if foe.isDead():
+                foe.enem_hp = 0
+                print("You deal", player_one.damage, "points of damage")
+                print("The enemy has", foe.enem_hp, "health left")
+                print("The enemy is dead")
+            else:
+                print("You deal", player_one.damage, "points of damage")
+                print("The enemy has", foe.enem_hp, "health left")
+
+            print("")
+            if not foe.isDead():
+                foe.attack(player_one)
+                if player_one.hp <=0:
+                    player_one.hp = 0
+                    print("The enemy deals", foe.hit, "points of damage")
+                    print("You have", player_one.hp, "health left")
+                    print("You are dead")
+                    print("GAME OVER")
+                    exit()
+                else:
+                    print("The enemy deals", foe.hit,"points of damage")
+                    print("You have",player_one.hp,"health left")
 
 
 
@@ -60,4 +103,7 @@ def main():
 
 
 
-main():
+
+
+
+main()
