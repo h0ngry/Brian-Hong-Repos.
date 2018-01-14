1b1f2fac9ad1a335e769ffc64375e17ce6a006c7
Brian Hong <35075001+h0ngry@users.noreply.github.com>
2018-01-14 17:54:02 -0600

    Add files via upload
    added blocking and weaknesses/ advantages of different types of enemy or player

--- TurnGame.py
+++ TurnGame.py
@@ -3,28 +3,45 @@ import random
 #enemy class
 class enemy:
     #attributes
+    #If enemy or player is melee, then it will do more damage to ranged opponents.
+    #Ranged will do more damage to magic opponents.
+    #Magic will do more damage to melee opponents.
     def __init__(self):
         num = random.randint(1,3)
         self.enem_hp = 10
         if(num==1):
-            self.type = "melee"
+            self.type = "warrior"
+
         if(num==2):
-            self.type = "ranged"
+            self.type = "archer"
         if(num==3):
-            self.type = "magic"
+            self.type = "mage"
 
     #attack method
     def attack(self,player):
-
+        dmg_multiplier = 2
         self.hit = random.randint(1,3)
+
+        if self.type == 'warrior' and player.type == 'archer':
+            self.hit = self.hit * dmg_multiplier
+
+        if self.type == 'archer' and player.type == 'mage':
+            self.hit = self.hit * dmg_multiplier
+
+        if self.type == 'mage' and player.type == 'warrior':
+            self.hit = self.hit * dmg_multiplier
+
+
 
         player.hp -= self.hit
 
     #block method
     #blocks the player attack based on chance
+    #Enemy has a 20% chance of blocking
     def block(self):
         block_chance = random.randint(1,10)
-        if block_chance == 1:
+
+        if block_chance in {1,2}:
             return True
     # checks if enemy is dead
     def isDead(self):
@@ -43,17 +60,17 @@ class enemy:
 class player:
 
     #attributes
-    def __init__(self,type):
-        if (type == 1):
-            category = "melee"
+    def __init__(self,category):
+        if (category == 1):
+            self.type = "warrior"
         #moves
         #damage range
 
-        if (type == 2):
-            category = "ranged"
+        if (category == 2):
+            self.type = "archer"
 
-        if (type == 3):
-            category = "mage"
+        if (category == 3):
+            self.type = "mage"
 
         self.hp = 10
         self.score = 0
@@ -67,23 +84,49 @@ class player:
 
     #attack method
     def attack(self,enemy):
-        self.damage = random.randint(4,5)
-        enemy.enem_hp -= self.damage
+        self.hit = random.randint(4,5)
+        if self.type == 'warrior' and enemy.type == 'archer':
+            self.hit = self.hit * dmg_multiplier
+
+        if self.type == 'archer' and enemy.type == 'mage':
+            self.hit = self.hit * dmg_multiplier
+
+        if self.type == 'mage' and enemy.type == 'warrior':
+            self.hit = self.hit * dmg_multiplier
+        enemy.enem_hp -= self.hit
 
     #block method
     #blocks the enemy attack based on chance
+    #You have a 50% chance of blocking an attack
     def block(self):
         block_chance = random.randint(1,10)
-        if block_chance in {1,2,3,4,5,6,7}:
+        if block_chance in {1,2,3,4,5}:
             return True
 
 
 
 
 def main():
+
+    #INTRODUCTION part
     #Prompts user to choose their class
-    typ = eval(input("Which class do you want to be? Press 1 to be a Warrior. Press 2 to be an Archer. Press 3 to be a mage. "))
+    typ = eval(input("Which class do you want to be? Press 1 to be a Warrior. Press 2 to be an Archer. Press 3 to be a Mage. "))
+    while typ not in {1,2,3}:
+        typ = eval(input("Which class do you want to be? Press 1 to be a Warrior. Press 2 to be an Archer. Press 3 to be a Mage. "))
+    if typ == 1:
+        print("")
+        print("You choose to be a Warrior which has a damage boost against archers")
+        print("")
+    if typ == 2:
+        print("")
+        print("You choose to be an Archer which has a damage boost against mages")
+        print("")
+    if typ == 3:
+        print("")
+        print("You choose to be a Mage which has a damage boost against warriors")
+        print("")
     player_one = player(typ)
+
 
     #All within this while loop occurs while the player/user is still alive
     while not player_one.isDead():
@@ -93,17 +136,18 @@ def main():
             print("")
             atk = eval(input("Press 1 to attack "))
             while atk != 1:
-                atk = eval(input("Press 1 to attack"))
+                atk = eval(input("Press 1 to attack "))
             print("")
             player_one.attack(foe)
             if foe.block():
-                foe.enem_hp += player_one.damage
+                foe.enem_hp += player_one.hit
                 print("Enemy has blocked your attack")
+                print("The enemy has", foe.enem_hp, "health left")
             else:
                 if foe.isDead():
                     foe.enem_hp = 0
                     player_one.score += 1
-                    print("You deal", player_one.damage, "points of damage")
+                    print("You deal", player_one.hit, "points of damage")
                     print("The enemy has", foe.enem_hp, "health left")
                     print("The enemy is dead")
                     print("")
@@ -111,7 +155,7 @@ def main():
                     while resp !=1:
                         resp = eval(input("Press 1 to continue "))
                 else:
-                    print("You deal", player_one.damage, "points of damage")
+                    print("You deal", player_one.hit, "points of damage")
                     print("The enemy has", foe.enem_hp, "health left")
 
             print("")
@@ -121,6 +165,7 @@ def main():
                 if player_one.block():
                     player_one.hp += foe.hit
                     print("You blocked the enemy's attack")
+                    print("You have", player_one.hp, "health left")
                 else:
                     if player_one.hp <=0:
                         player_one.hp = 0
